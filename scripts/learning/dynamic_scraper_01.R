a60a9343d0f601317909fcd0cccea6126b4609ad
Ashish Dutt <ashish.dutt8@gmail.com>
2017-05-13 14:48:57 +0800

    initial commit

---
+++ scripts/learning/dynamic_scraper_01.R
@@ -0,0 +1,5 @@
+# Objective: Scarping dynamic webpages using rvest & RSelenium
+# script name: dynamic_scraper_01
+
+library(rvest)
+url<-html("http://www.visitithaca.com/attractions/wineries.html")
